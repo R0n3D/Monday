c07d897b9855a5d34cf7cf2a70766bfa7bad4283
Ron Fridman <ronfour20@gmail.com>
2024-04-11 13:01:04 +0300

    ''

---
+++ monday.js
@@ -0,0 +1,95 @@
+let arr = [1,2,3,4];
+
+// let arr = [1,2,3,4];
+// const func1 = (arr, value) =>
+// {
+//     arr.push(value)
+//     return arr;
+
+// }
+// console.log(func1(arr, 4));
+ 
+//2
+
+// let arr1 = [1,2,3,4];
+// const func2 = (arr1) =>
+// {
+// arr1.pop();
+// return arr1;
+
+// } 
+// console.log(func2(arr1));
+
+//3
+
+// let arr3 = [1,2,3,4];
+// const func3 = (arr3) =>
+// {
+//    arr3.unshift(0)
+//    return arr3; 
+// }
+// console.log(func3(arr3));
+
+//4 
+
+// let arr4 = [1,2,3,4];
+// const func4 = (arr4) =>
+// {
+//     arr4.shift();
+//     return arr4;
+// }
+// console.log(func4(arr4));
+
+//5
+ let arrl = ["rami", "ron", "daniel",""];
+
+//  const func5 = (arrl) =>
+//  {
+//  return arrl.map((word) =>word.toUpperCase());
+//  }
+
+// console.log(func5(arrl));
+
+//6 
+//  const func6 =(arr)=> arr.filter((num) => num%2==0)
+//  console.log(func6(arr));
+
+//7 
+// const func7 = (arrl, ron) =>
+// {
+//  return arrl.indexOf(ron);
+// }
+// console.log(func7(arrl,"ron"));
+
+// //8 
+// const func8 = (arrl,ron) =>
+// {
+//     return arrl.includes(ron);
+// }
+// console.log(func8(arrl, "ron"));
+
+//9
+// const func9 = (arrl)=> 
+// {
+// return arrl.reverse();
+// }
+// console.log(func9(arrl));
+
+
+//10 
+let arr10 = ["one", "two"]
+const func10 = (arr10) =>  {
+    
+}
+console.log(arr10.concat(arrl));
+
+
+
+
+
+
+
+
+
+
+
---
+++ test.js
@@ -0,0 +1,36 @@
+const data = [
+    { name: "John Doe", age: 25, city: "New York", district: "Manhattan" },
+    { name: "Jane Smith", age: 30, city: "Los Angeles", district: "Hollywood" },
+    { name: "Michael Johnson", age: 28, city: "Chicago", district: "Downtown" },
+    { name: "Emily Davis", age: 35, city: "San Francisco", district: "Financial District" },
+    { name: "William Brown", age: 40, city: "Seattle", district: "Capitol Hill" },
+    { name: "Sarah Wilson", age: 27, city: "Boston", district: "Back Bay" },
+    { name: "David Martinez", age: 33, city: "Houston", district: "Midtown" },
+    { name: "Jessica Anderson", age: 22, city: "Miami", district: "South Beach" },
+    { name: "Christopher Lee", age: 31, city: "Philadelphia", district: "Center City" },
+    { name: "Amanda Taylor", age: 29, city: "Dallas", district: "Uptown" },
+    { name: "Daniel Thomas", age: 26, city: "Atlanta", district: "Buckhead" },
+    { name: "Lauren Clark", age: 34, city: "Denver", district: "LoDo" },
+    { name: "Kevin Rodriguez", age: 32, city: "Phoenix", district: "Downtown" },
+    { name: "Megan Lewis", age: 23, city: "Portland", district: "Pearl District" },
+    { name: "Brian Walker", age: 37, city: "Austin", district: "Downtown" },
+    { name: "Rachel Hill", age: 24, city: "Nashville", district: "The Gulch" },
+    { name: "Jason Wright", age: 39, city: "San Diego", district: "Gaslamp Quarter" },
+    { name: "Stephanie King", age: 36, city: "Minneapolis", district: "Downtown West" },
+    { name: "Ryan Turner", age: 28, city: "Las Vegas", district: "The Strip" },
+    { name: "Elizabeth Scott", age: 31, city: "Orlando", district: "Lake Buena Vista" }
+  ];
+
+//   let newpeople = prompt("name");
+//   const arr = newpeople.split("-");
+//   const obj = {name:arr[0],age:arr[1],city:arr[2],district:arr[3]};
+//   data.push(obj);
+//   console.log(data);
+
+data.forEach((person)=> console.log(person))
+  const arrWord = word.split("-")
+  
+
+
+
+
